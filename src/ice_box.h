ec6c1c1b5519c4006942afae2bc3828c9420d70f
strozell <strozell@gmail.com>
2022-11-12 17:50:54 -0800

    wip

--- src/ice_box.h
+++ src/ice_box.h
@@ -5,11 +5,16 @@
 #define PIN_FAN_TACH (2)
 #define PIN_FAN_PWM (3)
 #define PIN_FAN_POWER (4)
+#define PIN_POT_FRONT (A1)
+#define PIN_SWITCH  (5)
 
 // FAN MODULE
-#define FAN_PWM_DC_MIN (0.2f)
-#define FAN_TACH_DEBOUNCE (20) //0 is fine for most fans, crappy fans may require 10 or 20 to filter out noise
-#define FAN_TACH_STUCK_THRESHOLD (500) // if no interrupts were received for 500ms, consider the fan as stuck and report 0 RPM
-
+#define FAN_TACH_DEBOUNCE (20U) // 0 is fine for most fans, crappy fans may require 10 or 20 to filter out noise
+#define FAN_TACH_STUCK_THRESHOLD (500U) // if no interrupts were received for 500ms, consider the fan as stuck and report 0 RPM
+#define FAN_TACH_RPM_INVALID  (0UL)
+#define FAN_PWM_OCR2A_TOP (79)
+#define FAN_PWM_DC_OFF (0U)
+#define FAN_PWM_DC_ON_MIN (20U)
+#define FAN_PWM_DC_ON_MAX (100U)
 
 #endif //ICE_BOX_H
--- src/src.ino
+++ src/src.ino
@@ -2,80 +2,140 @@
 // Sam Rozell
 // 23 July 2022
 
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
 #include "ice_box.h"
 
-// Configure Timer 2 (pin 3) to output 25kHz PWM. 
-// Pin 11 will be unavailable for output in this mode
-void setupTimer2( void ){
-    //Set PWM frequency to about 25khz on pin 3 (timer 2 mode 5, prescale 8, count to 79)
-    TIMSK2 = 0;
-    TIFR2 = 0;
-    TCCR2A = (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);
-    TCCR2B = (1 << WGM22) | (1 << CS21);
-    OCR2A = 79;
-    OCR2B = 0;
-}
-
-// equivalent of analogWrite on pin 3
-void setPWM2( float f ){
-    // limit input from FAN_PWM_DC_MIN to 1
-    f= (f<FAN_PWM_DC_MIN) ? FAN_PWM_DC_MIN : (f>1 ? 1 : f); 
-    OCR2B = (uint8_t)(79*f);
-}
-
-
-//Interrupt handler. Stores the timestamps of the last 2 interrupts and handles debouncing
-unsigned long volatile ts1=0,ts2=0;
-void tachISR() {
-    unsigned long m=millis();
-    if((m-ts2)>FAN_TACH_DEBOUNCE){
-        ts1=ts2;
-        ts2=m;
-    }
-}
-//Calculates the RPM based on the timestamps of the last 2 interrupts. Can be called at any time.
-unsigned long calcRPM(){
-    if(millis()-ts2<FAN_TACH_STUCK_THRESHOLD&&ts2!=0){
-        return (60000/(ts2-ts1))/2;
-    }else return 0;
-}
-
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 void setup() {
-  // enable fan power through relay
-  pinMode(PIN_FAN_POWER, OUTPUT);
-  digitalWrite(PIN_FAN_POWER, HIGH);
-
-  // setp fan PWM timer
-  pinMode(PIN_FAN_PWM, OUTPUT);
-  setupTimer2();
-  setPWM2(0.4f);
-
-  // setup fan tachometry
-  pinMode(PIN_FAN_TACH, INPUT_PULLUP);
-  //set tachISR to be triggered when the signal on the sense pin goes low
-  attachInterrupt(digitalPinToInterrupt(PIN_FAN_TACH), tachISR, FALLING); 
-  Serial.begin(9600); //enable serial so we can see the RPM in the serial monitor
+  BoardPinsConfigure();
+  FanPwmTimerConfigure();
+  FanPwmSetDutyCyclePercent(FAN_PWM_DC_OFF);
+  FanTachConfigure();
+  mlx.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
 
-  setPWM2(0.4f);
-  for (int i=0; i<10; i++) {
-    delay(100);
-    Serial.print("RPM:");
-    Serial.println(calcRPM());
+  // Capture new input
+  unsigned int new_duty_cycle = PotCalculateDutyCycle();
+  bool laptop_contact = SwitchIsClosed();
+  int fan_power_enable = laptop_contact; // If laptop is removed, turn off fan power
+
+  // Apply new input
+  digitalWrite(PIN_FAN_POWER, fan_power_enable);
+  FanPwmSetDutyCyclePercent(new_duty_cycle);
+
+  // Loop
+  TempSerialPrint();
+  FanTachSerialPrint();
+  delay(100);
+}
+
+/*
+* Calculates the Duty Cycle setting from Potentiometer position
+* Returns:
+*     duty_cycle_percent - integer percentage (0-100)
+* Note: allows 0-100, not ON_MIN-100
+*/
+unsigned int PotCalculateDutyCycle() {
+  int pot_value = analogRead(PIN_POT_FRONT);
+  Serial.println(pot_value);
+
+  // scale to duty cycle percent
+  long int duty_cycle_percent = round(pot_value * 100L / 1023L); // want 0-100 from 0-1023
+
+  return duty_cycle_percent;
+}
+
+bool SwitchIsClosed() {
+  return digitalRead(PIN_SWITCH);
+}
+
+void TempSerialPrint() {
+ Serial.print(mlx.readObjectTempC());
+ Serial.println(" C");
+ Serial.print(mlx.readObjectTempF());
+ Serial.println(" F");
+}
+
+void FanTachSerialPrint() {
+  Serial.print("RPM:");
+  Serial.println(FanTachCalculateRPM());
+}
+
+
+/*
+* Initializes mode for all pins on the board
+*/
+void BoardPinsConfigure() {
+  pinMode(PIN_POT_FRONT, INPUT);
+  pinMode(PIN_SWITCH, INPUT);
+  pinMode(PIN_FAN_POWER, OUTPUT);
+  pinMode(PIN_FAN_PWM, OUTPUT);
+  pinMode(PIN_FAN_TACH, INPUT_PULLUP);
+}
+
+
+/*
+* Configures Timer 2 (pin 3) to output 25kHz PWM
+* Note: Pin 11 will be unavailable for output in this mode
+*/
+void FanPwmTimerConfigure() {
+  TCCR2A = 0;   // TC2 Control Register A
+  TCCR2B = 0;   // TC2 Control Register B
+  TIMSK2 = 0;   // TC2 Interrupt Mask Register
+  TIFR2 = 0;    // TC2 Interrupt Flag Register
+  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);  // OC2B cleared/set on match when up/down counting, fast PWM
+  TCCR2B |= (1 << WGM22) | (1 << CS21);     // prescaler 8
+  OCR2A = FAN_PWM_OCR2A_TOP;   // TOP overflow value (Hz)
+  OCR2B = 0;
+}
+
+/*
+* Sets a new Duty Cycle value for Fan PWM
+* equivalent of analogWrite on pin 3
+* Parameters:
+*     percent - integer percentage (0-100) for desired duty cycle
+* Note: percents below the minimum fan DC will turn the fan off
+*/
+void FanPwmSetDutyCyclePercent( unsigned int percent ){
+  // limit percent to safe range
+  percent = (percent < FAN_PWM_DC_ON_MIN) ? FAN_PWM_DC_OFF : (percent > FAN_PWM_DC_ON_MAX ? FAN_PWM_DC_ON_MAX : percent);
+
+  // scale to FAN_PWM_OCR2A_TOP setting
+  uint8_t ocr2b = FAN_PWM_OCR2A_TOP * percent * 0.01f;
+
+  OCR2B = ocr2b;
+}
+
+
+// Interrupt handler. Stores the timestamps of the last 2 interrupts and handles debouncing
+unsigned long volatile ts1 = 0, ts2 = 0;
+void TachISR() {
+  unsigned long m = millis();
+  if( (m - ts2) > FAN_TACH_DEBOUNCE ) {
+    ts1 = ts2;
+    ts2 = m;
   }
-  setPWM2(0.6f);
-  for (int i=0; i<10; i++) {
-    delay(100);
-    Serial.print("RPM:");
-    Serial.println(calcRPM());
+}
+
+/*
+* Configures Fan tachometer pin interrupt
+*/
+void FanTachConfigure() {
+  attachInterrupt(digitalPinToInterrupt(PIN_FAN_TACH), TachISR, FALLING); 
+}
+
+/* 
+* Calculates the RPM based on the timestamps of the last 2 interrupts.
+* Can be called at any time
+*/
+unsigned long FanTachCalculateRPM() {
+  if ( (millis() - ts2) < FAN_TACH_STUCK_THRESHOLD && ts2 != 0 ) {
+    return ( 60000 / (ts2 - ts1) ) / 2;
   }
-  setPWM2(0.8f);
-  for (int i=0; i<10; i++) {
-    delay(100);
-    Serial.print("RPM:");
-    Serial.println(calcRPM());
-  }
+  return FAN_TACH_RPM_INVALID;
 }
